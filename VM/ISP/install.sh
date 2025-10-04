db3f69a0153b621c102837f18d4123161a74569f
lik <117955767+l1qued@users.noreply.github.com>
2025-10-04 20:26:54 +0900

    Update install.sh

--- VM/ISP/install.sh
+++ VM/ISP/install.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-echo "L1qued-SH: Копирую папки config файлов..."
-# cp -r / /
-# cp -r / /
-# cp -r / /
+echo "L1qued-SH: Настраиваю hostname"
+hostnamectl set-hostname isp
+echo "L1qued-SH: Настраиваю часовой пояс"
+timedatectl set-timezone Asia/Yekaterinburg
 
-echo "L1qued-SH: Устанавливаю hostname"
-hostnamectl set-hostname isp; exec bash
 
-echo "L1qued-SH: Перезапускаю сеть"
+echo "L1qued-SH: Настраиваю интерфейсы"
+cat > /etc/network/interfaces << EOF
+source /etc/network/interfaces.d/*
+
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+
+auto eth1
+iface eth1 inet static
+address 172.16.4.1/28
+
+auto eth2
+iface eth2 inet static
+address 172.16.5.1/28
+EOF
+
+echo "L1qued-SH: Перезапускай службу сети"
 systemctl restart networking
 
-sleep 3
+echo "L1qued-SH: Включаю форвард пакеты"
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
 
-echo "L1qued-SH: Устанавливаю временную зону"
-timedatectl set-timezone Asia/Yekaterinburg
 
 echo "L1qued-SH: Настраиваю iptables"
 iptables -t nat -A POSTROUTING -s 172.16.4.0/28 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 172.16.5.0/28 -o eth0 -j MASQUERADE
-
-echo "L1qued-SH: Сохраняю правила iptables"
 iptables-save > /root/rules
 
-echo "L1qued-SH: Машина настроена"
+
+echo "L1qued-SH: Создаю правило crontab"
+(crontab -l 2>/dev/null; echo "@reboot /sbin/iptables-restore < /root/rules") | crontab -
+
+echo "L1qued-SH: Машина настроена!"
 echo "L1qued-SH: Отчистка истории ввода команд"
 
 history -c
-echo "L1qued-SH: Готово!"
+echo "L1qued-SH: Готово"
+echo "L1qued-SH: tg: @l1queds"
