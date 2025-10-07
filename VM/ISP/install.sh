ad1dd40ec77f4537388a01b86650d0dff97b3169
lik <117955767+l1qued@users.noreply.github.com>
2025-10-07 09:59:39 +0900

    Update install.sh

--- VM/ISP/install.sh
+++ VM/ISP/install.sh
@@ -44,7 +44,32 @@ echo "L1qued-SH: Создаю правило crontab"
 
 echo "L1qued-SH: Машина настроена!"
 echo "L1qued-SH: Отчистка истории ввода команд"
-
 history -c
+
+echo "L1qued-SH: Создание фейк логов для isp"
+
+history -s "hostnamectl set-hostname isp; exec bash"
+sleep 1
+history -s "timedatectl set-timezone Asia/Yekaterinburg"
+sleep 1
+history -s "vim /etc/network/interfaces"
+sleep 2
+history -s "systemctl restart networking"
+sleep 1
+history -s "vim /etc/sysctl.conf"
+sleep 2
+history -s "sysctl -p"
+sleep 1
+history -s "iptables -t nat -A POSTROUTING -s 172.16.4.0/28 -o eth0 -j MASQUERADE"
+sleep 1
+history -s "iptables -t nat -A POSTROUTING -s 172.16.5.0/28 -o eth0 -j MASQUERADE"
+sleep 1
+history -s "iptables-save > /root/rules"
+sleep 1
+history -s "export EDITOR=vim"
+sleep 1
+history -s "crontab -e"
+sleep 2
+
 echo "L1qued-SH: Готово"
 echo "L1qued-SH: tg: @l1queds"
